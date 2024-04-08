4b8ea9f6f52f4438cfbebf890246ed46c2a453a7
fariosofernando <farioso.f@gmail.com>
2024-04-08 03:33:43 +0200

    update

--- packages/user_package/src/controller.ts
+++ packages/user_package/src/controller.ts
@@ -1,19 +1,9 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-// import { getUserById } from "database_package/src/controller";
+import { getUserById, getAllUsers } from "database_package/src/controller";
 
 export const getUser = (req: Request, res: Response): void => {
-	// getUserById("123").then((result) => {
-	// 	if (!result.isSuccess) {
-	// 		return res
-	// 			.status(404)
-	// 			.json({ message: "Usuário não encontrado!", data: result.error });
-	// 	}
-
-	// 	return res.status(200).json({ result: result.value });
-	// });
-
 	res.status(200).json({ message: "Usuário encontrado com sucesso!" });
 };
 
